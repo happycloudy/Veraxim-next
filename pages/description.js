75cd86efc9801ffa76913dd0cae452ec46abcddf
happycloudy <arion.mikolae71@mail.ru>
2020-10-22 17:29:06 +0300

    Added Header Component

--- pages/description.js
+++ pages/description.js
@@ -1,10 +1,10 @@
-import Head from 'next/head'
+import Header from './header'
 import Footer from './footer'
 
 export default function Description() {
   return (
     <div className="container" style={{width: "50vw"}}>
-      <Head>
+      {/* <Head>
         <title>Описание</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"></link>
@@ -12,7 +12,8 @@ export default function Description() {
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-      </Head>
+      </Head> */}
+      <Header title="Описание"/>
 
     <div className="row description mt-5" >
       <div className="text col">
--- pages/requisites.js
+++ pages/requisites.js
@@ -1,10 +1,12 @@
 import Head from 'next/head'
+import Footer from './footer'
 
-export default function Requisites() {
+export default function Requisites(title) {
+
   return (
     <div className="container">
       <Head>
-        <title>Главная | Veraxim</title>
+        <title>123</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"></link>
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
@@ -12,6 +14,8 @@ export default function Requisites() {
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
       </Head>
 
+
+    <Footer/>
     </div>
   )
 }
